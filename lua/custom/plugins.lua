ddb8f0d6a11386ba4e8e2357d123c05e9ded31bd
Sahid Rahman <sahidrahman404@gmail.com>
2022-12-18 11:28:05 +0700

    add plugins

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,24 @@
+return function(use)
+  use { -- auto close
+    "m4xshen/autoclose.nvim",
+  }
+
+  use {
+    "jose-elias-alvarez/null-ls.nvim",
+    requires = { "nvim-lua/plenary.nvim" },
+  }
+
+  use { "lukas-reineke/lsp-format.nvim" }
+
+  use { "ggandor/lightspeed.nvim" }
+
+  use { "ThePrimeagen/harpoon" }
+
+  use { "ur4ltz/surround.nvim" }
+
+  use { "rose-pine/neovim" }
+
+  use { "akinsho/toggleterm.nvim", tag = '*', config = function()
+    require("toggleterm").setup()
+  end }
+end
