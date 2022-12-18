810b7aa4a0152e7d8576c7e565c831b8e4fb0648
Sahid Rahman <sahidrahman404@gmail.com>
2022-12-18 23:06:19 +0700

    add configuration for fomatting on save with sync format

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -1,4 +1,18 @@
 local null_ls = require "null-ls"
+
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format {
+    filter = function(client)
+      -- apply whatever logic you want (in this example, we'll only use null-ls)
+      return client.name == "null-ls"
+    end,
+    bufnr = bufnr,
+  }
+end
+
+-- if you want to set up formatting on save, you can use this as a callback
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup {
   sources = {
     null_ls.builtins.formatting.stylua,
@@ -6,27 +20,17 @@ null_ls.setup {
     null_ls.builtins.formatting.prettier,
     null_ls.builtins.completion.spell,
   },
+  -- add to your shared on_attach callback
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          lsp_formatting(bufnr)
+        end,
+      })
+    end
+  end,
 }
-
--- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
--- require("null-ls").setup {
---   -- you can reuse a shared lspconfig on_attach callback here
---   on_attach = function(client, bufnr)
---     if client.supports_method "textDocument/formatting" then
---       vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
---       vim.api.nvim_create_autocmd("BufWritePre", {
---         group = augroup,
---         buffer = bufnr,
---         callback = function()
---           -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
---           vim.lsp.buf.format {
---             bufnr = bufnr,
---             filter = function()
---               return client.name == "null-ls"
---             end,
---           }
---         end,
---       })
---     end
---   end,
--- }
